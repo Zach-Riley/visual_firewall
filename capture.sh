7255e0f9270453a19912de3b30a01e44c92005e6
gp24681 <ghp130130@utdallas.edu>
2016-04-28 20:35:13 -0500

    pcap capture script
    loops tcpdump command to capture packets and save to a group of rotating files, named VFCapture_year:month:date:fileNumber.pcap.
    rotates to next file after either 100000 packets have been captured or 5 minutes has elapsed, whichever comes first.

--- capture.sh
+++ capture.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+
 #The script simply runs the tcpdummp command and stores the packets captured to
 #a rotating file. The name of the file will be VFCaptureALL along with a timestamp
-#in the format year:month:date. It listens on all interfaces and rotates every
-#30 minutes.
+#in the format year:month:date:current file rotation. It listens on all interfaces
+#and rotates every 5 minutes. It will make up to 10 files starting at 0.
 
 DATE=`date +%Y:%m:%d`
-FILE="VFCaptureAll_"$DATE".pcap"
-#tcpdump -Z root -W 3 -C 3 -G 15 -w $FILE -i any #attempted to create circular window with this by having rotating files, but still overwrites single file. need to look more into this.
-tcpdump -G 1800 -w $FILE -i any
+FILE="VFCaptureAll_"$DATE
+
+FCOUNTER=0
+
+while :
+do
+    tcpdump -i any -c 100000 -G 300 -w ${FILE}:${FCOUNTER}.pcap
+    FCOUNTER=${FCOUNTER+1}
+    if [$FCOUNTER -ge 10];
+    then
+        FCOUNTER=0
+    fi
+done
+
+#captures up to 100000 packets or changes files after 5 minutes

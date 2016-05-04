f89d10c6f12f212a1baaab5b972c0b6fe3bb9c66
gp24681 <ghp130130@utdallas.edu>
2016-05-03 23:40:34 -0500

    Added function to call pcapConvert
    I use the -z option of tcpdump to call the pcapConvert script when the tcpdump is done filling a file and closes it.

--- capture.sh
+++ capture.sh
@@ -5,6 +5,8 @@
 #a rotating file. The name of the file will be VFCaptureALL along with a timestamp
 #in the format year:month:date:current file rotation. It listens on all interfaces
 #and rotates every 5 minutes. It will make up to 10 files starting at 0.
+#Upon completing and closing a file, it also calls the pcapConvert script to convert
+#the created pcap to a .har file to be viewed by the front end.
 
 DATE=`date +%Y:%m:%d`
 FILE="VFCaptureAll_"$DATE
@@ -13,7 +15,7 @@ FCOUNTER=0
 
 while :
 do
-    tcpdump -i any -c 100000 -G 300 -w ${FILE}:${FCOUNTER}.pcap
+    tcpdump -i any -c 100000 -G 300 -w ${FILE}:${FCOUNTER}.pcap -z ./pcapConvert.sh
     FCOUNTER=${FCOUNTER+1}
     if [$FCOUNTER -ge 10];
     then
@@ -22,3 +24,5 @@ do
 done
 
 #captures up to 100000 packets or changes files after 5 minutes
+~
+
